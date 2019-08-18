e3be6617bcb7260a4d9e2a47859a72b2b62dcf95
nilenuru <zagoraevav@gmail.com>
2019-08-18 23:19:32 +0300

    8 блок готов

---
+++ src/blocks/modules/kit/kit.js
@@ -0,0 +1,13 @@
+modules.define('kit', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
